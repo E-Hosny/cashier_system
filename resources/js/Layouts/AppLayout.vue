81775a80066f6477ec71764c8736ef83b68e045c
test <ebrahimhosny511@gmail.com>
2025-07-12 16:20:32 +0300

    .

--- resources/js/Layouts/AppLayout.vue
+++ resources/js/Layouts/AppLayout.vue
@@ -23,10 +23,6 @@ const switchToTeam = (team) => {
 };
 
 const logout = () => {
-    // حذف الكوكيز الخاصة بالجلسة يدويًا
-    document.cookie.split(';').forEach(function(c) {
-        document.cookie = c.trim().split('=')[0] + '=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;';
-    });
     router.post(route('logout'));
 };
 </script>
